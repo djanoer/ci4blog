6516cecf15db3b6a5a58566390ace88b690a77c5
Dave-Id <djanur405@gmail.com>
2024-07-17 16:03:32 +0800

    login form
    menambahkan halaman login form

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,11 +10,13 @@ $routes->get('/', 'Home::index');
 $routes->group('admin', static function($routes){
 
   $routes->group('', [], static function($routes){
-    $routes->view('example-page','example-page');
+    // $routes->view('example-page','example-page');
+    $routes->get('home','AdminController::index',['as'=>'admin.home']);
   });
 
   $routes->group('', [], static function($routes){
-    $routes->view('example-auth','example-auth');
+    // $routes->view('example-auth','example-auth');
+    $routes->get('login','AuthController::loginForm',['as'=>'admin.login.form']);
   });
 
 });
