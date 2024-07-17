68c9ad483f431e8d6139afc1dc47f3775a59b19a
megalodon-source <dave.id@tutamail.com>
2024-07-18 04:50:02 +0700

    Admin login process with validation
    membuat admin login proses dengan validasi dan menambahkan file baru. (Hash.php dan CIAuth.php)

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -3,7 +3,9 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
+use App\Libraries\CIAuth;
+use App\Libraries\Hash;
+use App\Models\User;
 
 class AuthController extends BaseController
 {
@@ -16,5 +18,11 @@ class AuthController extends BaseController
             'validation'=>null,
         ];
         return view('backend/pages/auth/login', $data);
+    }
+
+    public function loginHandler() {
+        $fieldType = filter_var($this->request->getVar('login_id'), FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        echo $fieldType;
     }
 }
--- app/Views/backend/pages/auth/login.php
+++ app/Views/backend/pages/auth/login.php
@@ -5,20 +5,47 @@
   <div class="login-title">
     <h2 class="text-center text-primary">Login</h2>
   </div>
-  <form>
-
+  <?php $validation = \Config\Services::validation(); ?>
+  <form action="<?= route_to('admin.login.handler') ?>" method="POST">
+    <?= csrf_field(); ?>
+    <?php if(!empty(session()->getFlashdata('success'))) : ?>
+      <div class="alert alert-success">
+        <?= session()->getFlashdata('success'); ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true" >&times;</span>
+        </button>
+      </div>
+    <?php endif; ?>
+    <?php if(!empty(session()->getFlashdata('fail'))) : ?>
+      <div class="alert alert-danger">
+        <?= session()->getFlashdata('fail') ?>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true" >&times;</span>
+        </button>
+      </div>
+    <?php endif; ?>
     <div class="input-group custom">
-      <input type="text" class="form-control form-control-lg" placeholder="Username">
+      <input type="text" class="form-control form-control-lg" placeholder="Username or Email" name="login_id" value="<?= set_value('login_id') ?>">
       <div class="input-group-append custom">
         <span class="input-group-text"><i class="icon-copy dw dw-user1"></i></span>
       </div>
     </div>
+    <?php if($validation->getError('login_id')) : ?>
+      <div class="d-block text-danger" style="margin-top:-25px; margin-bottom:15px;">
+        <?= $validation->getError('login_id') ?>
+      </div>
+    <?php endif; ?>
     <div class="input-group custom">
-      <input type="password" class="form-control form-control-lg" placeholder="**********">
+      <input type="password" class="form-control form-control-lg" placeholder="**********" name="password" value="<?= set_value('password') ?>">
       <div class="input-group-append custom">
         <span class="input-group-text"><i class="dw dw-padlock1"></i></span>
       </div>
     </div>
+    <?php if($validation->getError('password')) : ?>
+      <div class="d-block text-danger" style="margin-top:-25px; margin-bottom:15px;">
+        <?= $validation->getError('password'); ?>
+      </div>
+    <?php endif; ?>
     <div class="row pb-30">
       <div class="col-6">
         <div class="custom-control custom-checkbox">
@@ -35,11 +62,9 @@
     <div class="row">
       <div class="col-sm-12">
         <div class="input-group mb-0">
-          <!--
-											use code for form submit
-											<input class="btn btn-primary btn-lg btn-block" type="submit" value="Sign In">
-										-->
-          <a class="btn btn-primary btn-lg btn-block" href="index.html">Sign In</a>
+
+          <input class="btn btn-primary btn-lg btn-block" type="submit" value="Sign In">
+
         </div>
         <div class="font-16 weight-600 pt-10 pb-10 text-center" data-color="#707373" style="color: rgb(112, 115, 115);">
           OR
